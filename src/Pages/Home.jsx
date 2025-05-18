12b1354fc592ec0f2a10bec42c5b20bbf3f84a04
Dheeraj Patidar <patidardheeraj1944@gmail.com>
2025-05-18 13:06:28 +0530

    Change in Home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -79,7 +79,7 @@ const Home = () => {
 
 			<div>
 				<span>
-					<p className="tittle">FEATURED BRAND</p>
+					<p className="tittle">FEATURED BRANDS</p>
 				</span>
 
 				<div className="brands">
